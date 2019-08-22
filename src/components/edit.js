8957e7c8e9a9392ffc4eadf2ff88843f7341bdc2
Ranindu Jaysooriya <43095375+Ranindu15@users.noreply.github.com>
2019-08-22 19:24:10 +0530

    create edit.js

---
+++ src/components/edit.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+import {BrowserRouter as Route, Router, Link, Switch} from 'react-router-dom';
+
+class Edit extends Component {
+    render() {
+        return (
+            <div>
+                <Router>
+                    <div className="container">
+
+                    </div>
+                </Router>
+            </div>
+        );
+    }
+}
+
+export default Edit;
